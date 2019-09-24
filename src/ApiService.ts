c76facb5762c6e6de49044320ad9e1e1c6759fc7
Margarita Krutikova <margaretkru@gmail.com>
2019-09-24 14:00:28 +0200

    Fix link to api

--- src/ApiService.ts
+++ src/ApiService.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 import { SearchResult } from "./Types"
 
-const apiUrl = `https://www.superheroapi.com/api.php/${process.env.API_KEY}`
+const apiUrl = "https://www.superheroapi.com/api.php/1291558501027144"
 
 export const searchSuperHeroes = async (term: string) => {
   const { data } = await axios.get<SearchResult>(`${apiUrl}/search/${term}`)
